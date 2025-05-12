fcf60ee702deda0d07ac4e292ceca6d411b448f8
katarina862 <s32341@pjwstk.edu.pl>
2025-05-12 13:44:20 +0200

    kolejna wersja

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
@@ -6,6 +6,7 @@ using UnityEngine;
 public class GameController : MonoBehaviour
 {
     public string Word = "after";
+    public int LivesCount = 10;
     public TextMeshProUGUI MainWord;
     public TextMeshProUGUI OurInput;
     public TextMeshProUGUI SubmittedImput;
@@ -14,12 +15,65 @@ public class GameController : MonoBehaviour
     void Start()
     {
         MainWord.text = Word;
+        OurInput.text = $"Zostało prób: {LivesCount}."; 
     }
 
     public void OnInputChanged(string input)
     {
-        OurInput.text = input;
+        //OurInput.text = input;
     }
+
+    public void OnClick()
+    {
+        Debug.Log("Przycisk został kliknięty.");
+        LivesCount = LivesCount - 1;
+        OurInput.text = $"Zostało prób: {LivesCount}.";
+
+        if (Word == InputField.text)
+        {
+            Debug.Log($"Corect guess");
+            return;
+        }
+        if(Word.Length != InputField.text.Length)
+        {
+            SubmittedImput.text = $"Długość się nie zgadza";
+            return;
+        }
+
+        int bullsCount = CountBulls();
+        int cowsCount = CountCows();
+
+        SubmittedImput.text = $"Bulls: {bullsCount} and Cows:{cowsCount}";
+
+    }
+
+    private int CountCows()
+    {
+        int result = 0;
+
+        for (int i = 0; i < Word.Length; i++)
+        {
+            if (Word[i] != InputField.text[i] && Word.Contains(InputField.text[i]))
+            {
+                result++;
+            }
+
+            return result;
+    }
+    public int CountBulls()
+    {
+        int result = 0;
+
+        for (int i = 0; i < Word.Length; i++)
+        {
+            if(Word[i] == InputField.text[i])
+                {
+                  result++;
+                }
+        }
+        return result;
+    }
+
 
     // Update is called once per frame
     void Update()
